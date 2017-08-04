26d0efe58264ffcf860c6a6be64abe725efdca6a
curveball <x.curveball.x@gmail.com>
2017-08-04 22:01:56 +0300

    Default behaviour and appearence are set

--- js/tooltiper.js
+++ js/tooltiper.js
@@ -26,51 +26,80 @@
     $.fn.toolTiper = function(options) {
       var settings = {
         tooltipType: 'text', // html
-        tooltipCss: null,    // styles
-        tooltipDisappear: false,
+        tooltipDisappear: false, // *
+        tooltipAppearenceMode: 'fadeIn',
+        tooltipDisappearenceMode: 'fadeOut',
         tooltipOffset: 10,
-        tooltipBound: 'element', // cursor
+        tooltipBound: 'element', // *cursor
         tooltipShowSpeed: 'fast',
         tooltipHideSpeed: 'fast',
         tooltipClass: "js-tooltiper",
-        tooltipElement: "span"
+        tooltipElement: "span",
+        tooltipCss: {"display": "none", "white-space": "nowrap", "color": "black", "font-size": ".8em", "position": "absolute", "z-index": 9999, "background-color": "white", "padding": ".5em", "box-shadow": "0px 0px 4px 0px rgba(0,0,0,0.5)"}    // styles
       },
-      title = null,
-      selector = $(this).selector;
+      title = null, selector = $(this).selector;
 
       $.extend(settings, options);
 
-      $("body").on( "mouseenter", selector, function() {
-        showToolTip( $(this) );
-      }).on( "mouseleave", selector, function() {
+      $("body").on( "mouseenter", selector, function(event) {
+        showToolTip( $(this), event );
+      }).on( "mouseleave", selector, function(event) {
         hideToolTip( $(this) );
       });
-      
-      function showToolTip(element) {
-        if(isToolTipShown(element)) return;
+
+      function showToolTip(element, event) {
         title = element.attr('title');
-        if(!title) return;
+        if(isToolTipShown(element) || !title) return;
         element.attr('title', "");
-        var tooltip = createToolTip(title);
+        var tooltip = createToolTip(title, setTooltipCoords(element));
         element.append(tooltip);
-        tooltip.show(settings.tooltipShowSpeed);
+        tooltip[settings.tooltipAppearenceMode](settings.tooltipShowSpeed);
       }
       function hideToolTip(element) {
-        if(!isToolTipShown(element)) return;
-        getToolTip(element).hide(settings.tooltipHideSpeed);
+        if(!isToolTipShown(element) || !title) return;
         element.attr('title', title);
         title = null;
-        element.find(settings.tooltipElement + "." + settings.tooltipClass).remove();
+        getToolTip(element)[settings.tooltipDisappearenceMode](settings.tooltipHideSpeed, function() {
+          element.find(settings.tooltipElement + "." + settings.tooltipClass).remove();
+        });
       }
-      function createToolTip(title) {
-        var tooltip = $("<" + settings.tooltipElement + ">").addClass(settings.tooltipClass).css({ "display": "none"}).text(title);
-        return tooltip;
+      function createToolTip(title, coords) {
+        return $("<" + settings.tooltipElement + ">").addClass(settings.tooltipClass).text(title).css($.extend(settings.tooltipCss, {"top": coords.top, "left": coords.left}));
       }
       function getToolTip(element) {
         return element.find(settings.tooltipElement + "." + settings.tooltipClass);
       }
       function isToolTipShown(element) {
         return element.find(settings.tooltipElement + "." + settings.tooltipClass).length !== 0;
+      }
+      function setTooltipCoords(element) {
+        var positionedParent = getPositionedParent(element);
+        var toolTipXCoord = getToolTipXCoord(event, positionedParent);
+        var toolTipYCoord = getToolTipYCoord(element[0], positionedParent) + settings.tooltipOffset + element.height();
+        return { top: toolTipYCoord, left: toolTipXCoord };
+      }
+      function getToolTipXCoord(event, positionedParent) {
+        return positionedParent ? event.pageX - getElementCoords(positionedParent).left : event.pageX;
+      }
+      function getToolTipYCoord(element, positionedParent) {
+        return positionedParent ? getElementCoords(element).top - getElementCoords(positionedParent).top : getElementCoords(element).top;
+      }
+      function getPositionedParent(element) {
+        var parents = element.parents(), positions = ['absolute', 'relative', 'fixed'], positionedParent = null;
+        $.each(parents, function(index, parent) {
+          if(~$.inArray($.trim(parent.style.position), positions)) { positionedParent = parent; return; }
+        });
+        return positionedParent;
+      }
+      function getElementCoords(element) {
+        var box = element.getBoundingClientRect(), body = document.body, docEl = document.documentElement;
+        var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+        var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+        var clientTop = docEl.clientTop || body.clientTop || 0;
+        var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+        var top = box.top + scrollTop - clientTop;
+        var left = box.left + scrollLeft - clientLeft;
+        return { top: top, left: left };
       }
     }
 })(jQuery);
